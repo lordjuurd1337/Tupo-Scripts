7c93733c40b2e4e7c80486d96dbf18c6d526d56c
1337 <127702544+lordjuurd1337@users.noreply.github.com>
2024-06-03 22:35:57 +0100

    Create Support.lua

---
+++ Support.lua
@@ -0,0 +1,7 @@
+if LOADER["Rayfield"] then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kenniel123/Warrior-Simulator-BETA-/main/Warrior%20Simulator%20%5BBETA%5D%20RayField"))()
+end
+
+if LOADER["ArrayField"] then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kenniel123/Warrior-Simulator-BETA-ArrayField/main/Warrior%20Simulator%20%5BBETA%5D%20ArrayField"))()
+end
